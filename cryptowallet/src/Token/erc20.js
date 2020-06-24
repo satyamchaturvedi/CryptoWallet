f28d35516533c5d0adaf37f0d030ec889abc3e90
satyam <satyamchaturvedi217@gmail.com>
2020-06-24 11:21:59 -0700

    Added js files to components part of reactJS based Wallet app

---
+++ cryptowallet/src/Token/erc20.js
@@ -0,0 +1,5 @@
+import DCoin from './DCoin';
+const Tk20 = {
+    DCoin
+};
+export default Tk20;
---
+++ cryptowallet/src/Token/erc721.js
@@ -0,0 +1,5 @@
+import Condos from './Condos';
+const Tk721 = {
+    Condos
+};
+export default Tk721;
