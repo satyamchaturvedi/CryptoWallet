0585e37e9a96f4cd53360efa765b45f8a42a75d2
satyam <satyamchaturvedi217@gmail.com>
2020-06-24 08:33:49 -0700

    Adding the Migrated and deployed Contracts

--- migrations/1593003229_condos_migration.js
+++ migrations/1593003229_condos_migration.js
@@ -1,4 +1,4 @@
-let DCoin = artifacts.require("Condos.sol");
+let Condos = artifacts.require("Condos.sol");
 module.exports = function(_deployer) {
   // Use deployer to state migration tasks.
   _deployer.deploy(Condos);
